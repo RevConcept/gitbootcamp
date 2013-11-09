5850c517556449b9981b007c01311655979f8195
Chris Perryman <chris@revelationconcept.com>
2013-11-09 13:47:33 -0800

    new line added

--- file1.php
+++ file1.php
@@ -1,7 +1,8 @@
 <div>
 	<h1>Awesome Test</h1>
 	<p>Why is GIT so awesome? Look at all of these magical changes!</p>
-	<p>this is line 3</p>
 </div>
 
 <div><p>this is a new section</p></div>
+
+CHRIS ADDED THIS
